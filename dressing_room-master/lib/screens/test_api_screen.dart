1ed7ff5bf292f73ef793d9bf7b096d81ff752a27
Isaq0711 <isaquecsoares@gmail.com>
2023-11-03 11:51:03 -0300

    slider on test api

--- dressing_room-master/lib/screens/test_api_screen.dart
+++ dressing_room-master/lib/screens/test_api_screen.dart
@@ -1,13 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'dart:typed_data';
 import 'dart:async';
-import 'package:flutter/material.dart';
 import 'package:dressing_room/providers/user_provider.dart';
 import 'package:dressing_room/models/user.dart';
 import 'package:provider/provider.dart';
 import 'package:dressing_room/widgets/select_image_dialog.dart';
 import 'package:dressing_room/utils/colors.dart';
 import 'basket_screen.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class TestAPIScreen extends StatefulWidget {
   const TestAPIScreen({Key? key}) : super(key: key);
@@ -20,9 +20,14 @@ class _TestAPIScreenState extends State<TestAPIScreen> {
   List<Uint8List>? _files;
   List<Uint8List>? _bigfiles;
   bool isLoading = false;
+  int _currentPageIndex = 0;
+  PageController _pageController = PageController(initialPage: 0);
   bool _showNetworkImage = false;
   Timer? _timer;
   List<int> selectedIndexes = [];
+  late double _panelHeightOpen;
+  late double _panelHeightClosed;
+  late bool _isPanelVisible;
 
   @override
   void initState() {
@@ -30,6 +35,7 @@ class _TestAPIScreenState extends State<TestAPIScreen> {
     WidgetsBinding.instance!.addPostFrameCallback((_) {
       _selectBigImage(context);
     });
+    _isPanelVisible = false;
   }
 
   void _showNetworkImageOnLongPress() {
@@ -72,7 +78,7 @@ class _TestAPIScreenState extends State<TestAPIScreen> {
         return SelectImageDialog(
           onImageSelected: (Uint8List file) {
             setState(() {
-              _bigfiles = [];
+              _bigfiles ??= [];
               _bigfiles!.add(file);
             });
           },
@@ -81,64 +87,11 @@ class _TestAPIScreenState extends State<TestAPIScreen> {
     );
   }
 
-  Widget buildGridViewItem(int index) {
-   
-    final isSelected = selectedIndexes.contains(index);
-    if (index == 0) {
-      return InkWell(
-        onTap: () => _selectImage(context),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.transparent,
-              width: 3.0,
-            ),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child: Image.network(
-              'https://www.creativefabrica.com/wp-content/uploads/2019/05/Add-icon-by-ahlangraphic-1-580x386.jpg',
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      );
-    } else {
-      return GestureDetector(
-        onTap: () {
-          setState(() {
-            if (isSelected) {
-              selectedIndexes.remove(index);
-            } else {
-              selectedIndexes.add(index);
-            }
-          });
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: isSelected ? AppTheme.vinho : Colors.transparent,
-              width: 3.0,
-            ),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child:  index > 0 && _files != null && _files!.isNotEmpty
-                    ? Image.memory(
-                        _files![index - 1],
-                        fit: BoxFit.cover,
-                      )
-                    : Container(),
-          ),
-        ),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    _panelHeightOpen = MediaQuery.of(context).size.height * 0.7;
+    _panelHeightClosed = MediaQuery.of(context).size.height * 0.05;
+
     return Consumer<UserProvider>(
       builder: (context, userProvider, _) {
         var user = userProvider.getUser;
@@ -159,92 +112,242 @@ class _TestAPIScreenState extends State<TestAPIScreen> {
         : Scaffold(
             appBar: AppBar(
               backgroundColor: AppTheme.vinho,
-              title: const Text('Test API' , style: AppTheme.subheadlinewhite,
+              title: const Text(
+                'Test API',
+                style: TextStyle(color: Colors.white),
               ),
-              
             ),
-            body: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                   Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.55,
-                          width: double.infinity,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10.0),
-                            
-                            child: GestureDetector(
-                              onLongPress: () {
-                                _showNetworkImageOnLongPress();
-                              },
-                               onLongPressEnd: (details) {
-                                  _resetNetworkImage();
-                                },
-                              child: _showNetworkImage
-                                  ? Image.network(
-                                      'https://i0.wp.com/inspi.com.br/wp-content/uploads/2019/11/bliss-Charles-ORear.jpg?fit=1000%2C804&ssl=1',
-                                      fit: BoxFit.cover,
-                                      height: double.infinity,
-                                    width: double.infinity,
-                                    )
-                                  : Image.memory(
-                                      _bigfiles![0],
-                                      fit: BoxFit.cover,
-                                      height: double.infinity,
-                                    width: double.infinity,
+            body: Stack(
+              children: [
+                SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      isLoading ? LinearProgressIndicator() : const SizedBox(height: 0.0),
+                      const Divider(),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          _bigfiles != null
+                              ? SizedBox(
+                                  height: MediaQuery.of(context).size.height * 0.55,
+                                  child: PageView.builder(
+                                    controller: _pageController,
+                                    itemCount: _bigfiles!.length,
+                                    onPageChanged: (int index) {
+                                      setState(() {
+                                        _currentPageIndex = index;
+                                      });
+                                    },
+                                    itemBuilder: (context, pageIndex) {
+                                      return ClipRRect(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        child: GestureDetector(
+                                          onLongPress: () {
+                                            _showNetworkImageOnLongPress();
+                                          },
+                                          onLongPressEnd: (details) {
+                                            _resetNetworkImage();
+                                          },
+                                          child: _showNetworkImage
+                                              ? Container(
+                                                  height: MediaQuery.of(context).size.height * 0.55,
+                                                  width: MediaQuery.of(context).size.width,
+                                                  child: Image.network(
+                                                    'https://i0.wp.com/inspi.com.br/wp-content/uploads/2019/11/bliss-Charles-ORear.jpg?fit=1000%2C804&ssl=1',
+                                                    fit: BoxFit.cover,
+                                                  ),
+                                                )
+                                              : Container(
+                                                  height: MediaQuery.of(context).size.height * 0.55,
+                                                  width: MediaQuery.of(context).size.width,
+                                                  child: Image.memory(
+                                                    _bigfiles![pageIndex],
+                                                    fit: BoxFit.cover,
+                                                  ),
+                                                ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                )
+                              : Container(),
+                          Positioned(
+                            top: MediaQuery.of(context).size.height * 0.47,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            child: Container(
+                              child: CustomScrollView(
+                                scrollDirection: Axis.horizontal,
+                                slivers: [
+                                  SliverGrid(
+                                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                                      maxCrossAxisExtent: 180,
+                                      crossAxisSpacing: 20,
+                                      mainAxisSpacing: 10,
                                     ),
+                                    delegate: SliverChildBuilderDelegate(
+                                      (BuildContext context, int index) {
+                                        return GestureDetector(
+                                          onTap: () {
+                                            setState(() {
+                                              _currentPageIndex = index;
+                                              _pageController.animateToPage(
+                                                index,
+                                                duration: const Duration(milliseconds: 300),
+                                                curve: Curves.ease,
+                                              );
+                                            });
+                                          },
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              border: Border.all(
+                                                color: _currentPageIndex == index ? AppTheme.vinho : Colors.transparent,
+                                                width: 3.0,
+                                              ),
+                                              borderRadius: BorderRadius.circular(10.0),
+                                            ),
+                                            child: ClipRRect(
+                                              borderRadius: BorderRadius.circular(10.0),
+                                              child: Image.memory(
+                                                _bigfiles![index],
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                      childCount: _bigfiles!.length,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () => _selectBigImage(context),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            const Icon(
+                              Icons.add,
+                              color: Colors.black,
+                            ),
+                            const SizedBox(width: 4),
+                            const Text(
+                              'Add More Photos',
+                              style: TextStyle(
+                                fontFamily: 'Quicksand',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                   Container(
-                  height: MediaQuery.of(context).size.height * 0.008), 
-                 
-              Container(
-                  height: MediaQuery.of(context).size.height * 0.13,
-                  child: CustomScrollView(
-                    scrollDirection: Axis.horizontal,
-                    slivers: [
-                      SliverGrid(
-                        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 200,
-                          childAspectRatio: 3 / 2,
-                          crossAxisSpacing: 20,
-                          mainAxisSpacing: 10,
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.008),
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.13,
+                        child: CustomScrollView(
+                          scrollDirection: Axis.horizontal,
+                          slivers: [
+                            SliverGrid(
+                              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                                maxCrossAxisExtent: 200,
+                                childAspectRatio: 3 / 2,
+                                crossAxisSpacing: 20,
+                                mainAxisSpacing: 10,
+                              ),
+                              delegate: SliverChildBuilderDelegate(
+                                (BuildContext context, int index) {
+                                  return GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        _currentPageIndex = index;
+                                        _pageController.animateToPage(
+                                          index,
+                                          duration: const Duration(milliseconds: 300),
+                                          curve: Curves.ease,
+                                        );
+                                      });
+                                    },
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: _currentPageIndex == index ? AppTheme.vinho : Colors.transparent,
+                                          width: 3.0,
+                                        ),
+                                        borderRadius: BorderRadius.circular(10.0),
+                                      ),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        child: Image.memory(
+                                          _bigfiles![index],
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                                childCount: _bigfiles!.length,
+                              ),
+                            ),
+                          ],
                         ),
-                        delegate: SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                            return buildGridViewItem(index);
-                          },
-                          childCount: (_files?.length ?? 0) + 1,
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(AppTheme.vinho),
+                            ),
+                            onPressed: () {
+                              // TODO: Implement logic for check out
+                            },
+                            child: const Text(
+                              'TRY OUTFIT',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
-                 Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(AppTheme.vinho),
+                SlidingUpPanel(
+      
+                  color: AppTheme.vinho,
+                  maxHeight: _panelHeightOpen,
+                  minHeight: _panelHeightClosed,
+                  parallaxEnabled: true,
+                  parallaxOffset: 0.5,
+                  panel: BasketScreen(),
+                  collapsed: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isPanelVisible = !_isPanelVisible;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppTheme.vinho,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24.0),
+                          topRight: Radius.circular(24.0),
                         ),
-                        onPressed: () {
-                          // TODO: Implement logic for check out
-                        },
-                        child: const Text('TRY OUTFIT', style: AppTheme.subtitlewhite,),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.remove),
                       ),
                     ),
                   ),
-                   
+                  body: Container(),
+                ),
               ],
             ),
-            
-          ),
-        );
-}}
+          );
+  }
+}
